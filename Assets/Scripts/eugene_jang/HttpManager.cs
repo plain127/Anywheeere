a72f7786ef4a4078ec05679049d746b04c11a839
MadJangE00 <66401908+MadJangE00@users.noreply.github.com>
2024-08-29 18:36:43 +0900

    0829 first commit
    network setting

--- Assets/Scripts/eugene_jang/HttpManager.cs
+++ Assets/Scripts/eugene_jang/HttpManager.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;  // http 통신을 위한 네임 스페이스
 using System.Text;      // json, csv 같은 문서 형태의 인코딩  (UTF-8)을 위한 네임 스페이스
+using UnityEngine.UI;
+
 
 public class HttpManager : MonoBehaviour
 {
     public string url;
+    public Text text_response;
+    public RawImage img_response;
 
-    void Start()
+    public void Get()
     {
         StartCoroutine(GetRequest(url));
     }
@@ -28,16 +32,47 @@ public class HttpManager : MonoBehaviour
             // 응답받은 데이터를 출력한다.
             string response = request.downloadHandler.text;
             print(response);
+            text_response.text = response;
+
         }
         // 그렇지 않다면...(400,404 etc)
         else
         {
             // 에러 내용을 출력한다.
             print(request.error);
+            text_response.text = request.error;
         }
 
 
+    }
+    public void GetImage()
+    {
+        StartCoroutine(GetImageRequest(url));
+    }
 
+    IEnumerator GetImageRequest(string url)
+    {
+        // get(Texture) 통신을 준비한다.
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        // 서버에 요청을 하고 , 응답이 있을 때까지 기다린다.
+        yield return request.SendWebRequest();
+
+        // 만일, 응답이 성공이라면...
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            // 받은 텍스쳐 데이터를 Textur2D 변수에 받아 놓는다.
+            Texture2D response = DownloadHandlerTexture.GetContent(request);
+            // Texture2D 이미지를 img_response의 texture 값으로 넣어둔다.
+            img_response.texture = response;
+
+            // text_response에 성공 코드 번호를 출력한다.
+            text_response.text = "성공 - " + request.responseCode.ToString();
+
+        }else
+        {
+            print(request.error);
+            text_response.text = request.error;
+        }
     }
 
 }
---
+++ Assets/Scripts/eugene_jang/JsonParser.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text;
+using System.IO;
+
+[System.Serializable]
+public struct UserData
+{
+    public string name;
+    public int age;
+    public string job;
+    public bool isMan;
+
+    public UserData(string name, int age, string job, bool isMan)
+    {
+        this.name = name;
+        this.age = age; 
+        this.job = job;
+        this.isMan = isMan;
+
+    }
+}
+
+public struct PointedPlace
+{
+    int latitude;
+    int longitude;
+    string city;
+    string landmark;
+    public PointedPlace(int latitude, int longitude, string city, string landmark)
+    {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.city = city;  
+        this.landmark = landmark;
+    }
+}
+
+public class JsonParser : MonoBehaviour
+{
+    public Text result_text;
+
+    void Start()
+    {
+        #region json 데이터를 만들고 저장하기
+        //// 구조체 인스턴스를 만든다.
+        //UserData user1 = new UserData("박원석", 44, "강사", true);
+        ////user1.name = "박원석";
+        ////user1.age = 44;
+        ////user1.job = "강사";
+        ////user1.isMan = true;
+
+        //// 구조체 데이터를 Json 형태로 변경한다.
+        //string jsonUser1 = JsonUtility.ToJson(user1, true);
+
+        //print(jsonUser1);
+        //result_text.text = jsonUser1;
+
+        //SaveJsonData(jsonUser1, Application.dataPath, "박원석.json");
+        //Application.streamingAssetsPath 모바일의 경우 상대 경로
+        #endregion
+
+        string readString = ReadJsonData(Application.dataPath, "박원석json");
+        print(readString);
+    }
+
+    // text 데이터를 파일로 저장하기
+    public void SaveJsonData(string json, string path, string fileName)
+    {
+        // 1. 파일 스트림을 쓰기 형태로 연다.
+        //string fullPath = path + "/" + fileName;
+        string fullPath = Path.Combine(path, fileName);
+        FileStream fs = new FileStream(fullPath ,FileMode.OpenOrCreate, FileAccess.Write);
+        // 2. 스트림에 json 데이터를 쓰기로 전달한다.
+        byte[] jsonBinary = Encoding.UTF8.GetBytes(json);
+        fs.Write(jsonBinary);
+
+        // 3. 스트림을 닫아 준다.
+        fs.Close();
+    }
+
+    // text 파일을 읽어오기
+    public string ReadJsonData(string path, string fileName)
+    {
+        string readText;
+
+        //1. 파일 스트림을 읽기 모드로 연다
+        string fullPath = Path.Combine(path, fileName);
+        FileStream fs = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+
+        // 2. 스트림으로부터 데이터(byte)를 읽어 온다.
+        StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+        readText = sr.ReadToEnd();
+
+        // 3. 읽은 데이터를 string으로 변환해서 반환한다.
+        return readText;
+    }
+}
