4ffbe09c4fef5b14bdf65662f56f6ae918a4a39d
arcane <unitypractice37@gmail.com>
2024-09-22 22:59:41 +0900

    멀티 오류 수정

--- Assets/Scripts/sehun_park/GotoCityAdv.cs
+++ Assets/Scripts/sehun_park/GotoCityAdv.cs
@@ -5,13 +5,37 @@ using Photon.Pun;
 
 public class GotoCityAdv : MonoBehaviour
 {
+    private PhotonView photonView;
+
+    void Awake()
+    {
+        // PhotonView 컴포넌트를 찾고 초기화
+        photonView = GetComponent<PhotonView>();
+
+        // photonView가 null인지 확인
+        if (photonView == null)
+        {
+            Debug.LogError("PhotonView 컴포넌트가 이 게임 오브젝트에 없습니다.");
+        }
+    }
+
     void Update()
     {
         // B 키가 눌렸는지 확인
-        if (Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.B) && PhotonNetwork.IsMasterClient)
         {
-            // Photon 네트워크를 통해 씬을 로드
-            PhotonNetwork.LoadLevel("03_CesiumSanFrancisco");
+            // 모든 플레이어에게 씬 변경 요청
+            if (photonView != null)
+            {
+                photonView.RPC("ChangeScene", RpcTarget.All, "03_CesiumSanFrancisco");
+            }
         }
+    }
+
+    [PunRPC]
+    void ChangeScene(string sceneName)
+    {
+        // 씬 로드
+        PhotonNetwork.LoadLevel(sceneName);
     }
 }
