0cecf97e3e4ec0ac32bf7426dfd620480845bd78
arcane37-hello <unitypractice37@gmail.com>
2024-09-26 18:03:14 +0900

    중간 세이브

--- Assets/Scripts/sehun_park/Bullet2.cs
+++ Assets/Scripts/sehun_park/Bullet2.cs
@@ -6,7 +6,7 @@ public class Bullet2 : MonoBehaviourPun
 {
     public float moveSpeed = 10;
 
-    // Rigidboby
+    // RigidBody
     Rigidbody rb;
 
     // 충돌 되었을 때 효과 Prefab
@@ -14,26 +14,14 @@ public class Bullet2 : MonoBehaviourPun
 
     public AudioClip explosionSound;
 
-
-    int score = 0;
-
     void Start()
     {
-        // 내것일때만 
-        //if(photonView.IsMine)
-        {
-            rb = GetComponent<Rigidbody>();
-            rb.velocity = transform.up * moveSpeed;
-        }
-    }
-
-    void Update()
-    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.up * moveSpeed;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // 내것일때만 
         if (photonView.IsMine)
         {
             // 플레이어와 충돌했는지 확인
@@ -43,46 +31,38 @@ public class Bullet2 : MonoBehaviourPun
                 HPSystem hpSystem = other.GetComponent<HPSystem>();
                 if (hpSystem != null)
                 {
-                    // HP 1 감소 (네트워크 상에서 적용)
+                    // HP 1 감소
                     hpSystem.UpdateHP(-1f);
 
-                    // 점수 1 증가
-                    ScoreManager.instance.AddScore(1);
+                    // 점수 1 증가 (ScoreManager에서 관리)
+                    ScoreManager.instance.AddScore(1);  // 점수 추가
                 }
             }
 
-            // 부딪힌 지점을 향해서 Raycast 하자.
+            // 폭발 효과 및 사운드 처리
             Ray ray = new Ray(Camera.main.transform.position, transform.position - Camera.main.transform.position);
             RaycastHit hit;
             Physics.Raycast(ray, out hit);
 
-            // 폭발효과를 만들자
             photonView.RPC(nameof(CreatExplo), RpcTarget.All, transform.position, hit.normal);
-
-            // 사운드 재생
             photonView.RPC(nameof(PlayExplosionSound), RpcTarget.All);
 
-            // 나를 파괴하자
+            // 나를 파괴
             PhotonNetwork.Destroy(gameObject);
         }
     }
 
-
     [PunRPC]
     void CreatExplo(Vector3 position, Vector3 normal)
     {
-        // 폭발효과 생성
         GameObject explo = Instantiate(exploFactory);
-        // 생성된 효과를 나의 위치에 위치시키자 
         explo.transform.position = position;
-        // 생성된 효과의 앞방향을 부딪힌 지점의 normal 바꾸자
         explo.transform.forward = normal;
     }
 
     [PunRPC]
     void PlayExplosionSound()
     {
-        // 사운드 재생
         AudioSource.PlayClipAtPoint(explosionSound, transform.position);
     }
 }
